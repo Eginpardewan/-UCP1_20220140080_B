03406f2ea31cb6957eb773012d7715cd2e9509c1
Egin Pardewan <eginpardewan@gmail.com>
2024-05-06 22:58:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    alert("Haiii!! Gimana kabarnya? Semoga baik-baik saja ya! Ingat, setiap langkah kecil yang kamu ambil tujuannya untuk meraih kesuksesan sangat berarti, jadi jangan ragu untuk terus maju. Di sini, semoga kamu bisa menemukan banyak inspirasi untuk mewujudkan impianmu. Jadi, nikmati saja perjalananmu di halaman ini!");
+    
+}
+
+//pdw2024
